88d6e42c23c24991de8de23b87dade1c9f62d0f7
Sravan Vanteru <svanteru@healthgrades.com>
2017-02-25 21:13:58 +0530

    location assign changes

--- _protected/backend/views/user/_form.php
+++ _protected/backend/views/user/_form.php
@@ -35,7 +35,7 @@ use yii\widgets\ActiveForm;
         <?php endforeach ?>
         <?= $form->field($role, 'item_name')->dropDownList($roles) ?>
 
-        <?= $form->field($user, 'location')->ListBox(array("Bangalore"=>"Bangalore","Mysore"=>"Mysore","Marathahalli"=>"Marathahalli"),array('multiple' => 'multiple')); ?> 
+        <?= $form->field($user, 'location')->ListBox(array("Bangalore South"=>"Bangalore South","Bangalore North"=>"Bangalore North"),array('multiple' => 'multiple')); ?> 
 
     </div>
     </div>
