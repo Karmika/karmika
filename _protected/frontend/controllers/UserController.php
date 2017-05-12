dbfed7adfcc5907e16f670c28dee731218a93d58
Sravan Vanteru <svanteru@healthgrades.com>
2017-05-12 10:50:43 +0530

    feed user data changes

--- _protected/frontend/controllers/UserController.php
+++ _protected/frontend/controllers/UserController.php
@@ -2,6 +2,13 @@
 namespace frontend\controllers;
 
 use yii\rest\ActiveController;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+use common\models\User;
 
 /**
  * Country Controller API
@@ -13,10 +20,19 @@ class UserController extends ActiveController
     public $modelClass = 'common\models\User';
 
     public function actionSaveuser(){
-    	
+
         $post = file_get_contents("php://input");
         $users = json_decode($post, true);
-        $users = $_POST;
-        return $users;
+        foreach ($users as $key => $user) {
+        	$user_exists = User::findByUsername($user['2']);
+        	if(!empty($user_exists) || $user_exists != null) continue;
+			$model = new User();
+			$model->attributes = array('username' => $user['2'],'status' => 10,'email' => $user['3'],'password' => $user['4']);
+			$model->save();
+	        $userUpdate = User::findOne($model->id);
+	        $userUpdate->password_hash = Yii::$app->security->generatePasswordHash($user['4']);
+	        $userUpdate->update();
+        }
+        return true;;
     }
 }
